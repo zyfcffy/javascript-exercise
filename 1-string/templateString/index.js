e23a223929ccf9ab551ca321966e3ebf2e9ea220
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-21 16:04:29 +0800

    [fufangyuan] complete string templateString

--- 1-string/templateString/index.js
+++ 1-string/templateString/index.js
@@ -6,5 +6,6 @@ function getScore(id) {
 }
 
 export default function getScoreOutput(person) {
-  // TODO 8: 在这里写实现代码
+  const score = getScore(person.id);
+  return `你好，${person.lastName}${person.firstName}！你的考试成绩为${score}分`;
 }

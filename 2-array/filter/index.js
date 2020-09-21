f8ae446a849e19c873dc9841991ca5975d0816b9
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-21 18:09:44 +0800

    [fufangyuan] complete array filter

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,9 @@
 function chooseMultiplesOfThree(collection) {
-  // TODO 1: 在这里写实现代码
+  return collection.filter(n => n % 3 === 0);
 }
 
 function chooseNoRepeatNumber(collection) {
-  // TODO 2: 在这里写实现代码
+  return collection.filter((index, item) => collection.indexOf(index) === item);
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };

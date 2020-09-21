42e2db4036d2d201f979a9f0b793245b78fae980
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-21 20:07:28 +0800

    [fufangyuan] complete class practice_3

--- 4-class/practice_3/student.js
+++ 4-class/practice_3/student.js
@@ -1,1 +1,12 @@
-// TODO 13: 在这里写实现代码
+import Person from './person';
+
+export default class Student extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    return `I am a Student. I am at Class ${this.klass}.`;
+  }
+}
--- 4-class/practice_3/worker.js
+++ 4-class/practice_3/worker.js
@@ -1,1 +1,7 @@
-// TODO 14: 在这里写实现代码
+import Person from './person';
+
+export default class Worker extends Person {
+  introduce() {
+    return `I am a Worker. I have a job.`;
+  }
+}

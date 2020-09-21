280e4f799b273798c5f096f624048c3e51b233e5
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-21 20:00:20 +0800

    [fufangyuan] complete class practice_2

--- 4-class/practice_2/person.js
+++ 4-class/practice_2/person.js
@@ -1,1 +1,10 @@
-// TODO 10: 在这里写实现代码
+export default class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  introduce() {
+    return `My name is ${this.name}. I am ${this.age} years old.`;
+  }
+}

8cce5bd7daae030786c9317c16525c0be41a945b
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-21 20:35:19 +0800

    [fufangyuan] complete class practice_5

--- 4-class/practice_5/person.js
+++ 4-class/practice_5/person.js
@@ -1,1 +1,14 @@
-// TODO 18: 在这里写实现代码
+export default class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  basicIntroduce() {
+    return `My name is ${this.name}. I am ${this.age} years old.`;
+  }
+
+  introduce() {
+    return this.basicIntroduce();
+  }
+}
--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,15 @@
-// TODO 20: 在这里写实现代码
+import Person from './person';
+
+export default class Teacher extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    if (this.klass === undefined) {
+      return `${super.basicIntroduce()} I am a Teacher. I teach No Class.`;
+    }
+    return `${super.basicIntroduce()} I am a Teacher. I teach Class ${this.klass}.`;
+  }
+}

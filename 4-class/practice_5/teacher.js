4d2ca9551c66400e2949f611cb5b0cd461ccf12d
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-22 10:05:45 +0800

    [fufangyuan] correct class code after code diff

--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -7,9 +7,8 @@ export default class Teacher extends Person {
   }
 
   introduce() {
-    if (this.klass === undefined) {
-      return `${super.basicIntroduce()} I am a Teacher. I teach No Class.`;
-    }
-    return `${super.basicIntroduce()} I am a Teacher. I teach Class ${this.klass}.`;
+    return `${super.basicIntroduce()} I am a Teacher. I teach ${
+      this.klass ? `Class ${this.klass}` : `No Class`
+    }.`;
   }
 }
